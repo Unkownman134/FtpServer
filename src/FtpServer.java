0507705b081746790d62458caf2cfc1f19450f75
God_of_Admin <O0ohasho0O@proton.me>
2025-06-20 17:01:35 +0800

    feat(FtpServer): 增加FtpServer类
    增加FtpServer类，它是FTP服务器的主入口点。它负责监听传入的客户端连接，并为每个连接创建一个新的处理程序线程。

---
+++ src/FtpServer.java
@@ -0,0 +1,12 @@
+public class FtpServer {
+    /**
+     * Java应用程序入口方法
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+        new FtpServer().start();
+    }
+
+    private void start() {
+    }
+}
