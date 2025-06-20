87499548d4dc23176a9b34c78bdb2274951bb2a6
God_of_Admin <O0ohasho0O@proton.me>
2025-06-20 17:22:18 +0800

    feat(FtpServer): 增加初始化逻辑
    完善FtpServer类，增加初始化逻辑。连接利用多线程，初始化线程池大小。

--- src/FtpServer.java
+++ src/FtpServer.java
@@ -1,4 +1,21 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class FtpServer {
+    // 定义FTP控制连接的默认端口号
+    private static final int CONTROL_PORT = 21;
+    // 线程池，用于并发处理多个客户端连接，以避免为每个客户端都创建一个新线程的开销
+    private ExecutorService clientThreadPool;
+    // 初始化线程池大小
+    private static final int DEFAULT_THREAD_POOL_SIZE = 10;
+
+    /**
+     * 构造函数
+     */
+    public FtpServer() {
+        clientThreadPool = Executors.newFixedThreadPool(DEFAULT_THREAD_POOL_SIZE);
+    }
+
     /**
      * Java应用程序入口方法
      * @param args 命令行参数
